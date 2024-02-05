50c8064c1b61d6944bd148cbad0a90dd735f83d2
Saulo Henrique Rodrigues <saulohrodrigues@gmail.com>
2024-02-05 17:47:56 -0300

    Desafio da tabuada. Realizado na linguagem Java.

---
+++ src/TabuadaUmNumero.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class TabuadaUmNumero {
+    public static void main(String[] args) {
+        Integer tabuada;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Tabuada de um Número");
+        System.out.println();
+        System.out.print("Digite a tabuada desejada: ");
+        tabuada = input.nextInt();
+        for(int i = 1;i<=10;i++){
+            System.out.print(tabuada + "x"+i +"="+tabuada*i + ", ");
+        }
+        input.close();
+    }
+}
