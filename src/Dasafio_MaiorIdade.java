0e9778c956c6e37d6947b50c405929d160810929
Saulo Henrique Rodrigues <saulohrodrigues@gmail.com>
2024-02-05 10:55:26 -0300

    Desafio maior idade. Realizado na linguagem Java.

---
+++ src/Dasafio_MaiorIdade.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Dasafio_MaiorIdade {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Integer idade;
+        String nome;
+        System.out.println("Verificador de maior idade.");
+
+        System.out.print("Digite seu nome: ");
+        nome= input.nextLine();
+        System.out.print("Digite sua idade: ");
+        idade = input.nextInt();
+        input.close();
+
+        if (idade >= 18){
+            System.out.print(nome+" você é maior de idade!");
+        }else{
+            System.out.println(nome +" você é menor de idade!");
+        }
+    }
+}
